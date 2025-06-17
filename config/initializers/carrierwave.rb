9c1b8700d776d55799c6a2b0ee42c624e966c5b1
MomonST <posy-today6d@icloud.com>
2025-06-17 12:01:56 +0900

    ACLの設定を戻す

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,11 +8,8 @@ CarrierWave.configure do |config|
       region:                'ap-northeast-1' # 東京リージョンの例
     }
     config.fog_directory  = ENV['AWS_S3_BUCKET']
-     # これを削除または明示的にオフ
-    # config.fog_public     = true
-
-    # ACLを付与しないよう明示的に設定（fog-aws推奨方法）
-    config.fog_attributes = { }  # 空にしてACLなし
+     
+    config.fog_public     = true
     config.storage        = :fog
   else
     config.storage = :file
