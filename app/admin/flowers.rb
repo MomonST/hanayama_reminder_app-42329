b048e7025cec073e3a0ca8df7481d32caf15b522
MomonST <posy-today6d@icloud.com>
2025-06-24 19:47:47 +0900

    事前準備（ActiveAdmin）

---
+++ app/admin/flowers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Flower do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :scientific_name, :bloom_start_month, :bloom_end_month, :description, :image_url
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :scientific_name, :bloom_start_month, :bloom_end_month, :description, :image_url]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/mountains.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Mountain do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :region, :difficulty_level, :elevation, :latitude, :longitude, :description, :image_url
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :region, :difficulty_level, :elevation, :latitude, :longitude, :description, :image_url]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
